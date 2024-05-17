b88a29a2e434c6520bbfeee17cb344f47d6d1190
marcoArCha <marcoarcha@hotmail.com>
2024-05-17 14:52:33 -0600

    Lab completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Remaining from './components/Remaining';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import ExpenseTotal from './components/ExpenseTotal';
+import Currency from './components/Currency';
 
 // Add code to import the other components here under
 
@@ -26,6 +27,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,30 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const updatedBudget = parseInt(event.target.value, 10);
+
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if (updatedBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+
+        setNewBudget(updatedBudget);
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: updatedBudget,
+        });
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
